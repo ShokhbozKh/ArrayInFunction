790d7ab47b92664dc9b31696e224af276706f6e1
Shohboz <shohboztuit@gmail.com>
2023-07-23 17:11:48 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] {1,2,3,4,5};
+
+            Console.WriteLine("array 1");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\nArray 2");
+            ArrayChange(array);
+
+
+        }
+
+        static void ArrayChange(int[] arr)
+        {
+            for (int i = 0; i< arr.Length; i++)
+            {
+                arr[i] = arr[i] * 2;
+            }
+            for(int i=0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+    }
+}
